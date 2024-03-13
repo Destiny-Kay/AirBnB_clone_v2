b2efd050345c6d38fff541dfd4ca3fca4ae0114b
Destiny-Kay <destinykevogo@gmail.com>
2024-03-13 14:14:47 +0300

    Add file compressor

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+from fabric.api import local
+from os.path import isdir
+from datetime import datetime
+
+
+def do_pack():
+    """Generates a tg archive of all files and folders"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+            file_name = "versions/web_static_{}.tgz".format(date)
+            local("tar -cvzf {} web_static".format(file_name))
+            return file_name
+    except:
+        return None
